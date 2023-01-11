885b0f1c2521ec797b3dcdb17e078b5a9e9ae76b
fel <felicien.dehertogh@protonmail.com>
2023-01-11 12:49:47 +0100

    add card image to BusinessFuture component

--- src/components/home/BusinessFuture/BusinessFuture.jsx
+++ src/components/home/BusinessFuture/BusinessFuture.jsx
@@ -1,12 +1,16 @@
+import Image from 'next/image';
 import GradientLink from '../../shared/GradientLink/GradientLink';
 
 const BusinessFuture = () => {
   return (
-    <div className="bg-black relative h-[350px] mt-[120px] text-white">
-      <div className="ml-5 mr-32 pt-8 text-xl">
-        Meet your business future starting from now with Cardy
+    <div className="bg-black relative h-[385px] mt-[120px] text-white">
+      <div className="ml-5 mr-[130px] pt-8 text-xl leading-8">
+        Meet Your Business Future Starting From Now With Cardy
       </div>
-      <GradientLink href="/" positionY="top-[280px]" />
+      <div className="absolute h-[260px] w-[270px] right-[-20px] top-[30px]">
+        <Image src="/assets/img/card.svg" fill alt="your card" />
+      </div>
+      <GradientLink href="/" positionY="top-[315px]" />
     </div>
   );
 };
