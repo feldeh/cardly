32c9773835ad26b8fa19bcf8542f21f9bd9d9e05
fel <felicien.dehertogh@protonmail.com>
2023-02-01 00:09:57 +0100

    responsive footer

--- src/components/all/Navbar/Footer/Footer.jsx
+++ src/components/all/Navbar/Footer/Footer.jsx
@@ -2,11 +2,11 @@ import GradientLink from '../../../shared/GradientLink/GradientLink';
 
 const Footer = () => {
   return (
-    <div className="relative bottom-0 w-full px-10 pt-10 pb-4 text-center text-white bg-black">
-      <p className="text-xl leading-[30px] mb-[50px]">
-        Connect with the Future Go Digital with Your Business Card
+    <div className="relative bottom-0 w-full px-10 pt-10 lg:pt-[210px] pb-4 lg:pb-[50px] text-center text-white bg-black">
+      <p className="text-xl lg:text-[70px] lg:leading-[80px] leading-[30px] mb-[50px] lg:mb-[80px]">
+        Connect with the Future <br /> Go Digital with Your Business Card
       </p>
-      <div className="mb-[40px]">
+      <div className="mb-[40px] lg:mb-[120px]">
         <GradientLink href="/waitlist" text="rejoindre la liste d'attente" />
       </div>
       <p className="text-[14px]">Cardly</p>
