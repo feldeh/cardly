78f8b9b9fed617b0eec1ed423f8ab20370599f9f
fel <felicien.dehertogh@protonmail.com>
2023-02-02 04:02:37 +0100

    large screen responsive Chiffres

--- src/components/home/Chiffres/Chiffres.jsx
+++ src/components/home/Chiffres/Chiffres.jsx
@@ -2,18 +2,45 @@ import SectionTitle from '../../shared/SectionTitle/SectionTitle';
 import ExperienceCards from './ExperienceCards/ExperienceCards';
 
 const Chiffres = () => {
+  const experiences1 = [
+    {
+      number: '13',
+      description: 'Années D’expériences',
+    },
+    {
+      number: '84',
+      description: 'Années D’expériences',
+    },
+  ];
+  const experiences2 = [
+    {
+      number: '30',
+      description: 'Années D’expériences',
+    },
+    {
+      number: '40K',
+      description: 'Années D’expériences',
+    },
+  ];
   return (
-    <div className="h-[960px] lg:h-[550px] mt-24 lg:mt-[250px] relative">
-      <SectionTitle
-        title="En Chiffres"
-        id="faq"
-        subTitle="Cardly En Un Coup"
-        subTitle2="D’oeil"
-        number="04"
-      />
+    <div className="h-[960px] lg:h-[550px] mt-24 lg:mt-[180px] largeScreen:flex largeScreen:flex-row lg:mb-20 largeScreen:mb-0">
+      <div className="w-full">
+        <SectionTitle
+          title="En Chiffres"
+          id="faq"
+          subTitle="Cardly En Un Coup"
+          subTitle2="D’oeil"
+          number="04"
+        />
+      </div>
 
-      <div className="absolute top-[118px] lg:top-[200px] w-full">
-        <ExperienceCards />
+      <div className=" top-[118px] lg:top-[200px] w-full largeScreen:flex largeScreen:flex-col largeScreen:items-center largeScreen:mr-28 xl:mr-[400px]">
+        <div className="lg:flex lg:justify-center">
+          <ExperienceCards experiences={experiences1} />
+        </div>
+        <div className="lg:flex lg:justify-center">
+          <ExperienceCards experiences={experiences2} />
+        </div>
       </div>
     </div>
   );
--- src/components/home/Chiffres/ExperienceCards/ExperienceCards.jsx
+++ src/components/home/Chiffres/ExperienceCards/ExperienceCards.jsx
@@ -1,29 +1,11 @@
-const ExperienceCards = () => {
-  const experiences = [
-    {
-      number: '13',
-      description: 'Années D’expériences',
-    },
-    {
-      number: '84',
-      description: 'Années D’expériences',
-    },
-    {
-      number: '30',
-      description: 'Années D’expériences',
-    },
-    {
-      number: '40K',
-      description: 'Années D’expériences',
-    },
-  ];
+const ExperienceCards = ({ experiences }) => {
   return (
     <div className="lg:flex lg:justify-between">
       {experiences.map((experience, i) => {
         return (
           <div
             key={i}
-            className="mx-8 mb-[33px] h-44 lg:w-[300px] bg-[#F3F3F3] rounded-[20px] text-center"
+            className="mx-8 macbook:mx-4 mb-[33px] h-44 lg:w-[300px] bg-[#F3F3F3] rounded-[20px] text-center"
           >
             <p className="text-[65px] tracking-wide text-transparent bg-clip-text purpleGradient w-fit mx-auto pt-6">
               {experience.number}+
