d954883e51f61f65f4ca8cd319b2274e54e8e61b
fel <felicien.dehertogh@protonmail.com>
2023-02-02 05:41:03 +0100

    remove HeroImg animation

--- src/components/home/Hero/BottomHeroImg/BottomHeroImg.jsx
+++ src/components/home/Hero/BottomHeroImg/BottomHeroImg.jsx
@@ -7,7 +7,7 @@ const BottomHeroImg = () => {
         <Image src="/assets/img/threeCards.svg" alt="cards" fill />
       </div>
       <div className="relative z-10 smartphone:hidden">
-        <div className="absoluteCenter left-[38%] top-[-60px] w-[94px] h-[94px] animate-rotation-slow">
+        <div className="absoluteCenter left-1/2 top-[-15px] w-[94px] h-[94px]">
           <Image src="/assets/img/decouvrir.svg" alt="decouvrir" fill />
         </div>
         <div className="w-full h-[40px]">
