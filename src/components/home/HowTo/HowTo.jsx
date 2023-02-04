2514131a93ca1391fd9979911eaa6f0e9589af7b
fel <felicien.dehertogh@protonmail.com>
2023-02-05 00:54:44 +0100

    fix responsive HowTo section

--- src/components/home/HowTo/HowTo.jsx
+++ src/components/home/HowTo/HowTo.jsx
@@ -14,18 +14,20 @@ const HowTo = () => {
         number="02"
         flex
       />
-      <div className="mt-[50px] bg-[#E8E8E7] relative lg:mx-[50px] lg:rounded-[55px] lg:pb-[130px] xl:mx-[300px] 2xl:mx-[500px]">
-        <div className="text-center text-[28px] leading-8 pt-10 pb-[150px] lg:text-[60px] lg:leading-[60px] lg:pt-16">
-          <p className="text-black font-ABeeZeeItalic">Comment fonctionne</p>
-          <p className="mx-auto text-transparent bg-clip-text purpleGradient w-fit">
-            Cardly
-          </p>
-        </div>
-        <div className="absoluteCenter h-[81px] w-[81px] lg:hidden">
-          <Image src="/assets/img/play.svg" fill alt="play" />
-        </div>
-        <div className="hidden absoluteCenter h-[200px] w-[200px] top-[230px] lg:block">
-          <Image src="/assets/img/playLarge.svg" fill alt="play" />
+      <div className="flex justify-center mt-[50px]">
+        <div className=" bg-[#E8E8E7] relative max-smartphone:w-full smartphone:rounded-[55px] lg:pb-[130px]">
+          <div className="mx-[50px] text-center text-[28px] leading-8 pt-10 pb-[150px] lg:mx-[200px] lg:text-[60px] lg:leading-[60px] lg:pt-16">
+            <p className="text-black font-ABeeZeeItalic">Comment fonctionne</p>
+            <p className="mx-auto text-transparent bg-clip-text purpleGradient w-fit">
+              Cardly
+            </p>
+          </div>
+          <div className="absoluteCenter h-[81px] w-[81px] lg:hidden">
+            <Image src="/assets/img/play.svg" fill alt="play" />
+          </div>
+          <div className="hidden absoluteCenter h-[200px] w-[200px] top-[230px] lg:block">
+            <Image src="/assets/img/playLarge.svg" fill alt="play" />
+          </div>
         </div>
       </div>
     </div>
