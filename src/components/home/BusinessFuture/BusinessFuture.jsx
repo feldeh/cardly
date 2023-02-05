0af73e60c9444f9c0c38ff67cf3dcaf700833e6f
fel <felicien.dehertogh@protonmail.com>
2023-02-05 02:01:56 +0100

    responsive BusinessFuture section for very large screen

--- src/components/home/BusinessFuture/BusinessFuture.jsx
+++ src/components/home/BusinessFuture/BusinessFuture.jsx
@@ -16,10 +16,10 @@ const BusinessFuture = () => {
     //   </div>
     // </div>
     <div className="bg-black mt-[120px] text-white w-full relative lg:px-[72px] lg:pb-[300px] xl:min-h-[748px]">
-      <div className="pt-8 ml-5 mr-[130px] text-xl leading-8 smartphone:w-5/12 smartphone:leading-[40px] smartphone:pt-[70px] sm:ml-12 sm:w-7/12 sm:text-[30px] sm:leading-[50px] lg:ml-0 lg:pt-48 lg:text-[50px] lg:leading-[65px] lg:w-3/6 lg:tracking-wide xl:pt-32 xl:text-[60px] xl:leading-[80px] xl:w-3/6 xl:tracking-wide">
+      <div className="pt-8 ml-5 mr-[130px] text-xl leading-8 smartphone:w-5/12 smartphone:leading-[40px] smartphone:pt-[70px] sm:ml-12 sm:w-7/12 sm:text-[30px] sm:leading-[50px] lg:ml-0 lg:pt-48 lg:text-[50px] lg:leading-[65px] lg:w-3/6 lg:tracking-wide xl:pt-32 xl:text-[60px] xl:leading-[80px] xl:w-3/6 xl:max-w-[800px] xl:tracking-wide">
         Meet Your Business Future Starting From Now With Cardly
       </div>
-      <div className="absolute h-[260px] w-[270px] right-0 top-[38px] smartphone:top-[30px] min-[570px]:right-[50px] sm:right-0 sm:h-[300px] sm:w-[400px] lg:h-[600px] lg:w-[689px] lg:right-[-40px] lg:top-[80px] xl:h-[700px] xl:w-[804px] xl:right-[70px] xl:top-[10px]">
+      <div className="absolute h-[260px] w-[270px] right-0 top-[38px] smartphone:top-[30px] min-[570px]:right-[50px] sm:right-0 sm:h-[300px] sm:w-[400px] lg:h-[600px] lg:w-[689px] lg:right-[-40px] lg:top-[80px] xl:h-[700px] xl:w-[804px] xl:right-[70px] xl:top-[10px] 2xl:right-[100px] min-[1800px]:right-[150px]">
         <Image src="/assets/img/card.svg" fill alt="your card" />
       </div>
 
